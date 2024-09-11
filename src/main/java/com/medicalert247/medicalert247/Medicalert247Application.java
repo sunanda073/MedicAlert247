39e88f2966d64022478a632d8010ace2af9c9d0a
Sunanda <sunandachakraborty270@gmail.com>
2024-09-11 08:28:11 +0530

    first commit

---
+++ src/main/java/com/medicalert247/medicalert247/Medicalert247Application.java
@@ -0,0 +1,13 @@
+package com.medicalert247.medicalert247;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Medicalert247Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Medicalert247Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/medicalert247/medicalert247/Medicalert247ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicalert247.medicalert247;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Medicalert247ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
